ea5c87d24763087ad38b877ff111171c22640d03
Claudia Werner <claudia.werner@inria.fr>
2013-01-31 10:18:46 +0100

    modified constructor in Delaunay_mesh_sphere_traits

--- Triangulation_on_sphere_2/include/CGAL/Delaunay_mesh_sphere_traits_2.h
+++ Triangulation_on_sphere_2/include/CGAL/Delaunay_mesh_sphere_traits_2.h
@@ -5,54 +5,51 @@
 #include <CGAL/number_utils_classes.h>
 #include <CGAL/triangulation_assertions.h>
 #include <CGAL/Kernel_traits.h>
-#include <CGAL/Line_2.h>
-#include <CGAL/Ray_2.h>
-#include <CGAL/Point_2.h>
-#include <CGAL/Segment_2.h>
-#include <CGAL/Triangle_2.h>
-#include <CGAL/Line_2.h>
-#include <CGAL/Ray_2.h>
 #include <CGAL/predicates_on_points_2.h>
 #include <CGAL/basic_constructions_2.h>
 #include <CGAL/distance_predicates_2.h>
 
 #include <CGAL/triangulation_assertions.h>
-#include <CGAL/Segment_2_Segment_2_intersection.h>
+
 
 
 namespace CGAL { 
-	/*
-template <class Base>
-class Compute_area_2{
+			
+template <class K, class Predicate_>
+class Project_on_sphere_adaptor{
 public:
-	typedef typename Base::Point_2   Point_2;
-	typedef typename Base::Kernel Kernel;
-	typedef double result_type;
-	result_type 
-	operator()( const Point_2& p, const Point_2& q, const Point_2& r ) const
-	{return compute_area_2_object()(p,q);}
+	
+	typedef Predicate_ Predicate;
+	typedef typename K::Point_3 Point;
+	Project_on_sphere_adaptor(double radius):_radius(radius){}
+double _radius;
+	typedef typename Predicate::result_type result_type;
 	
 	
-//protected:
-	//Point_2 _sphere;
-};
-*/
-/*
-template <class Base>
-class Construct_midpoint_2{
-public:
-	typedef typename Base::Point_2   Point_2;
-	
-	typedef Point_2 result_type;
-	result_type
-	operator()(const Point_2& p, const Point_2&q)
-	{
+	result_type operator()(const Point& p0, const Point& p1)  {
+		Point p = Predicate()(p0,p1);
+		return project(p);
+	}
 		
-		return midpoint(p,q);}
+	result_type operator()(const Point& p0, const Point& p1, const Point& p2)  {
+		Point p = Predicate()(p0,p1,p2);
+		return project(p);
+	}
 	
-	};*/
+private:
+	Point project(const Point& p)
+		{return project(p.x(), p.y(), p.z());}
+					   
+	Point project(double x, double y, double z){
+		double dist = x*x+y*y+z*z;
+		if (dist ==0) return Point(0,0,0);
+		double scale= _radius/sqrt(dist);
+		double test = sqrt((x*x+y*y+z*z)*scale*scale);
+		return Point(x*scale, y*scale, z*scale);
+	}
 	
-	
+
+};
 	
 template < class Base >
 class Delaunay_mesh_sphere_traits_2
@@ -60,26 +57,33 @@ class Delaunay_mesh_sphere_traits_2
 {
 public:
 	typedef Delaunay_mesh_sphere_traits_2<Base> Self;
-	//typedef typename CGAL::Compute_area_2<Self> Compute_area_2;
-	//typedef typename CGAL::Construct_midpoint_2<Self> Construct_midpoint_2;
 	typedef typename Base::Point_2 Point_2;
-		typedef typename Base::Kernel Kernel;
+	typedef typename Base::Kernel Kernel;
 	typedef typename Kernel::Vector_3 Vector_2;
 	typedef typename Kernel::Compute_area_3 Compute_area_2;
-	typedef typename Kernel::Construct_midpoint_3 Construct_midpoint_2;
 	typedef typename Kernel::Angle_3 Angle_2;
 	typedef typename Kernel::Construct_vector_3 Construct_vector_2;
 	typedef typename Kernel::Construct_scaled_vector_3 Construct_scaled_vector_2;
 	typedef typename Kernel::Construct_translated_point_3 Construct_translated_point_2;
+	
+	
+	typedef Project_on_sphere_adaptor<Kernel,typename Kernel::Construct_midpoint_3> Construct_midpoint_2;
+	typedef Project_on_sphere_adaptor<Kernel,typename Kernel::Construct_circumcenter_3> Construct_circumcenter_3;
+	
+	typedef boost::true_type requires_test;
+	double _radius;
+	double _minDistSquared;
+	void set_radius(double a)
+	{_radius = a;
+		_minDistSquared = (_radius*pow (2, -23))*(_radius*pow (2, -23));
+	}
+	double radius(){return _radius;}
+	Delaunay_mesh_sphere_traits_2(double radius=1);
+	
 	Compute_area_2
 	compute_area_2_object()const
 	{return Compute_area_2();}
-	
-	Construct_midpoint_2
-	construct_midpoint_2_object()const
-	{return Construct_midpoint_2();}
-	
-	
+		
 	Angle_2
 	angle_2_object()const
 	{return Angle_2();}
@@ -96,24 +100,24 @@ public:
 	construct_translated_point_2_object()const
 	{return Construct_translated_point_2();}
 	
-	/*Compute_area_2
-	compute_area_2_object()const
-	{return Compute_area_2();}
-	
+		
 	Construct_midpoint_2
 	construct_midpoint_2_object()const
-	{return Construct_midpoint_2();}*/
+	{return Construct_midpoint_2(_radius);}
+	
+	Construct_circumcenter_3
+	construct_circumcenter_2_object()const
+	{return Construct_circumcenter_3(_radius);}
 protected:
 	Point_2 _sphere;
 };	
+
+template <class R>
+	Delaunay_mesh_sphere_traits_2<R>::
+	Delaunay_mesh_sphere_traits_2(double radius)
+	:_radius(radius){}
 	
-/*
-template < class R >
-Delaunay_mesh_sphere_traits_2<R> ::
-Delaunay_mesh_sphere_traits_2(const Point_2& sphere)
-: _sphere(sphere)
-	{}
-*/
+	
 	
 } //namespace CGAL
 
