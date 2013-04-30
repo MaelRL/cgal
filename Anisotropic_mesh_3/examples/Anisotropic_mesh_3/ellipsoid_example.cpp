512024d47fdfe93923ad145909bd90781ca07af5
Jane Tournois <jane.tournois@geometryfactory.com>
2013-04-30 16:19:03 +0200

    add approximation criterion

--- Anisotropic_mesh_3/examples/Anisotropic_mesh_3/ellipsoid_example.cpp
+++ Anisotropic_mesh_3/examples/Anisotropic_mesh_3/ellipsoid_example.cpp
@@ -56,11 +56,12 @@ int main(int argc, char* argv[])
 
   K::FT r0 = (argc > 5) ? atof(argv[5]) : 0./*default*/;
   K::FT gamma0 = (argc > 6) ? atof(argv[6]) : 0.;
+  K::FT approx = (argc > 7) ? atof(argv[7]) : 0.;
 
-  int nb = (argc > 7) ? atoi(argv[7]) : 10;
+  int nb = (argc > 8) ? atoi(argv[8]) : 10;
 
-  K::FT beta = (argc > 8) ? atof(argv[8]) : 2.5;
-  K::FT delta = (argc > 9) ? atof(argv[9]) : 0.3;
+  K::FT beta = (argc > 9) ? atof(argv[9]) : 2.5;
+  K::FT delta = (argc > 10) ? atof(argv[10]) : 0.3;
 
   fx << "Ellipsoid :" << std::endl;
   fx << "\ta = " << a << std::endl;
@@ -69,6 +70,7 @@ int main(int argc, char* argv[])
   fx << "\teps = " << epsilon << std::endl;
   fx << "\tr0 = " << r0 << std::endl;
   fx << "\tgamma0 = " << gamma0 << std::endl;
+  fx << "\tapprox = " << approx << std::endl;
   fx << "\tbeta = " << beta << std::endl;
   fx << "\tdelta = " << delta << std::endl;
   
@@ -77,7 +79,9 @@ int main(int argc, char* argv[])
                                                     r0,     //circumradius_ 0.1
                                                     gamma0, //distortion_ 1.3
                                                     beta,   //beta_ 2.5
-                                                    delta); //delta_ 0.3
+                                                    delta, //delta_ 0.3
+                                                    60,
+                                                    approx);//approx
 
   fx << std::endl << "nbV" << "\t" << "time" << std::endl;
   timer.start();
