d14379d63e046d22acdf9ee3ffa0c6b60469115c
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2014-05-07 15:16:34 +0200

    Added forward declaration files of a polyhedral surface type to the demo

---
+++ Anisotropic_mesh_3/demo/Polyhedral_surface_type.h
@@ -0,0 +1,9 @@
+#ifndef POLYHEDRAL_SURFACE_TYPE_H
+#define POLYHEDRAL_SURFACE_TYPE_H
+
+#include "Polyhedron_type.h"
+#include "Polyhedral_surface_type_fwd.h"
+
+#include <CGAL/Constrain_surface_3_polyhedral.h>
+
+#endif // POLYHEDRAL_SURFACE_TYPE_H
