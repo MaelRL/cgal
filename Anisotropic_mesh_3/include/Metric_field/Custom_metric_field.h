06cd4ce25272897cb4bb93654937b7049a6914ff
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2014-05-28 19:12:42 +0200

    Added a "custom" metric field type to be used as a metric field sandbox

---
+++ Anisotropic_mesh_3/include/Metric_field/Custom_metric_field.h
@@ -0,0 +1,154 @@
+#ifndef CGAL_ANISOTROPIC_MESH_3_CUSTOM_METRIC_FIELD_H
+#define CGAL_ANISOTROPIC_MESH_3_CUSTOM_METRIC_FIELD_H
+
+#include <CGAL/Metric_field.h>
+
+namespace CGAL
+{
+namespace Anisotropic_mesh_3
+{
+
+template<typename K>
+class Custom_metric_field : public Metric_field<K>
+{
+public:
+  typedef Metric_field<K>           Base;
+  typedef typename Base::FT         FT;
+  typedef typename Base::Metric     Metric;
+  typedef typename Base::Point_3    Point_3;
+  typedef typename Base::Vector_3   Vector_3;
+
+public:
+  Metric yang_liu(const Point_3& p) const
+  {
+    //Yang Liu 3D Metric on a [1,11]^3 cube
+    double x = p.x();
+    double y = p.y();
+    double z = p.z();
+
+    double r = std::sqrt(x*x + y*y + z*z);
+
+    x /= r;
+    y /= r;
+    z /= r;
+
+    double h1 = 0.025 + (1-std::exp(-0.01 * std::abs(r*r-49)));
+    double h2 = 1.;
+    double h3 = 1.;
+
+    double v2y = -y*z / std::sqrt((x*x + y*y + z*z)*(x*x + y*y));
+    double v2x = x*v2y/y;
+    double v2z = std::sqrt(1-(x*x/(y*y)+1)*v2y*v2y);
+
+    Vector_3 v1(x,y,z);
+    Vector_3 v2(v2x, v2y, v2z);
+    Vector_3 v3 = CGAL::cross_product(v1, v2);
+
+    return this->build_metric(v1, v2, v3, h1, h2, h3);
+  }
+
+  virtual Metric compute_metric(const Point_3 &p) const
+  {
+    return yang_liu(p);
+
+    FT denom = std::sqrt(9.); // = ratio at x=+/-1
+    double h = 1.0;
+
+    //linear interpolation
+    /*
+    return this->build_metric(Vector_3(1, 0, 0),
+                              Vector_3(0, 1, 0),
+                              Vector_3(0, 0, 1),
+                              1./((denom*std::abs(p.x())+(1-std::abs(p.x())))*h), 1./h, 1./h);
+    */
+
+    //logexp 1D
+    return this->build_metric(Vector_3(1, 0, 0),
+                              Vector_3(0, 1, 0),
+                              Vector_3(0, 0, 1),
+                              std::pow(1./denom, std::abs(p.x()))/h, 1./h, 1./h);
+
+    //logexp 1D extended into uniform
+    return this->build_metric(Vector_3(1, 0, 0),
+                              Vector_3(0, 1, 0),
+                              Vector_3(0, 0, 1),
+                              std::max(std::pow(1./denom, std::abs(p.x())),1./denom), 1., 1.);
+
+/*
+    //Various interpolations
+    FT l = 10;
+    FT lambda = 1. - std::abs(p.x()) / l;
+    FT at0 = 1.; //value at lambda = 0
+    FT at1 = 1. / l; //value at lambda = 1
+    FT a  = at0 + (at1-at0)*lambda*lambda*(3.-2.*lambda); //smoothstep
+
+    //other interpolation: ev =  l^(-[2*]lambda) to have delta(x,y) == delta(y, z) if d(x,y) == d(y,z)
+    a = std::pow(l, lambda);
+    a = 1./(a);
+
+    return this->build_metric(Vector_3(1, 0, 0),
+                              Vector_3(0, 1, 0),
+                              Vector_3(0, 0, 1),
+                              a, 1., 1.);
+
+    FT x = 39.9667;
+    l = x/2.;
+    FT half_l = l/2.;
+    FT max_ev = (1./half_l)*(1./half_l); // sqrt(max_ev) = desired_max_ratio = 2/l
+
+    Point_3 pOy(0., p.y(), 0.); //projection on Oy
+    FT sq_d = CGAL::squared_distance(p, pOy);
+
+    FT ez = 1., ey = 1.;
+    Vector_3 vy(0., 1., 0.);
+    Vector_3 vx(pOy, p);
+
+    if(sq_d == 0.)
+      vx = Vector_3(1.,0.,0.);
+    else
+      vx = std::sqrt(1./(vx*vx)) * vx;
+
+    Vector_3 vz = CGAL::cross_product(vx, vy);
+
+    FT ex;
+    if(sq_d < half_l*half_l)
+      ex = max_ev;
+    else if(sq_d > (l+half_l)*(l+half_l))
+      ex = 1.;
+    else
+    {
+      FT lambda = (std::sqrt(sq_d)-l/2.)/l;
+
+      //linear interpolation
+      //ex = lambda + (1-lambda)*max_ev;
+
+      //log exp interpolation
+      ex = std::exp((1-lambda)*std::log(max_ev)); // if d==l/2., lambda = 0, ex = (2./l)²
+                                                    // if d==3l/2, lambda = 1, ex = 1
+      //smoothstep interpolation
+      FT aa = max_ev; //value at lambda = 0
+      FT bb = 1.; //value at lambda = 1
+      ex = aa + (bb-aa)*lambda*lambda*(3.-2.*lambda);
+    }
+
+    ey = 0.005;
+
+    Metric M = this->build_metric(vx, vy, vz, ex, ey, ez);
+    return M;
+*/
+  }
+
+  virtual void report(typename std::ofstream &fx) const 
+  {
+
+  }
+
+public:
+  Custom_metric_field(FT epsilon_ = 1e-6, FT en_factor = 0.999)
+  : Metric_field<K>(epsilon_, en_factor) { }
+};
+
+} //Aniso
+} //CGAL
+
+#endif
