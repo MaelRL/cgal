eaa3b55f7a5dc6782cb87613388c9428602f2bb1
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2015-01-28 12:37:14 +0100

    Added refinement point computations to Aniso_TC

--- Anisotropic_mesh_TC/include/CGAL/Star.h
+++ Anisotropic_mesh_TC/include/CGAL/Star.h
@@ -470,10 +470,363 @@ public:
     std::cout << "INSERT : " << index << " IN STAR " << m_center_v->data() << std::endl;
     std::cout << "finite cells: " << this->number_of_finite_full_cells() << std::endl;
     std::cout << "dim rt: " << this->current_dimension() << std::endl;
+    this->is_valid(true);
 
-    return v;
+    return vh;
   }
 
+  // Refinement ----------------------------------------------------------------
+  FT norm(const Point_D& p) const // squared norm, actually
+  {
+    FT n = 0.;
+    for(int i=0; i<D(); ++i)
+      n += p[i]*p[i];
+    return n;
+  }
+
+  bool is_support_intersecting(const std::vector<Star_handle>& cell,
+                               Point_D& sol) const
+  {
+    // intersection with the tangent plane at the corresponding point
+    // on the paraboloid of a point of a simplex
+
+    // system is given by
+    //   d(p,p0) - w0 = d(p,pi) - wi
+    //   p \in T_{P_0}
+
+    E_Matrix_D m = E_Matrix_D::Zero();
+    E_Vector_D l;
+    WPoint_D p0 = cell[0]->m_center_S;
+    FT d0 = norm(p0.point());
+    Point_D p0_on_Q = cell[0]->m_center_Q;
+
+//    std::cout << "w0: " << cell[0]->index() << " || ";
+//    std::cout << p0.point()[0] << " " << p0.point()[1] << " ";
+//    std::cout << p0.point()[2] << " " << p0.point()[3] << " ";
+//    std::cout << p0.point()[4] << " w " << p0.weight() << std::endl;
+
+//    std::cout << "base: ";
+//    std::cout << p0_on_Q[0] << " " << p0_on_Q[1] << std::endl;
+
+    //first part of the matrix (lines 0-d) (dual equation)
+    for(int i=0; i<d(); ++i)
+    {
+      WPoint_D pi = cell[i+1]->m_center_S;
+//      std::cout << "wi: " << cell[i+1]->index() << " || ";
+//      std::cout << pi.point()[0] << " " << pi.point()[1] << " ";
+//      std::cout << pi.point()[2] << " " << pi.point()[3] << " ";
+//      std::cout << pi.point()[4] << " w " << pi.weight() << std::endl;
+
+      FT di = norm(pi.point());
+      l(i) = d0 - p0.weight() - di + pi.weight();
+      for(int j=0; j<D(); ++j)
+        m(i,j) = 2*(p0.point()[j] - pi.point()[j]);
+    }
+
+    //second part of the matrix (lines d-D) (tangent plane equation)
+    for(int i=d(); i<D(); ++i)
+    {
+      l(i) = p0_on_Q[i];
+      m(i,i) = -1;
+    }
+
+    int pos = d();
+    for(int i=0; i<d(); ++i)
+    {
+      for(int j=i; j<d(); ++j)
+      {
+        if(i == j)
+          m(pos, i) = 2*(cell[0]->m_center)[i];
+        else
+        {
+          m(pos, i) = (cell[0]->m_center)[j];
+          m(pos, j) = (cell[0]->m_center)[i];
+        }
+        pos++;
+      }
+    }
+
+//    std::cout << "m @ supportintersection:" << std::endl << m << std::endl;
+//    std::cout << "LHS " << l.transpose() << std::endl;
+    FT det = m.determinant();
+//    std::cout << "The determinant of m is " << det << std::endl;
+
+    if(std::abs(det) > 1e-10)
+    {
+      E_Matrix_D mm1 = m.inverse();
+//      std::cout << "The inverse of m is:\n" << mm1 << std::endl;
+      E_Vector_D vsol = mm1*l;
+      sol = KD().construct_point_d_object()(D(), vsol.data(), vsol.data()+D());
+//      std::cout << "sol: " << vsol.transpose() << std::endl;
+
+//      for(int i=0; i<(d()+1); ++i)
+//      {
+//        const WPoint_D& wi = cell[i]->m_center_S;
+//        FT di = KD().squared_distance_d_object()(sol, wi.point()) - wi.weight();
+//        std::cout << "dist² sol " << cell[i]->index() << " : "  << di << std::endl;
+//      }
+      return true;
+    }
+    else
+    {
+      std::cout << "null det" << std::endl;
+      return false;
+    }
+  }
+
+  struct H // hyperplanes describing the dual and error computation
+  {
+    const std::vector<Star_handle>& cell;
+
+    const int d() const { return dDim::value; }
+    const int D() const { return DDim::value; }
+
+    E_Vector_d operator()(const E_Vector_d& s) const
+    {
+      Point_d sp = Kd().construct_point_d_object()(d(), s.data(), s.data()+d());
+      Point_D s_on_Q = cell[0]->to_Q(sp);
+
+//      std::cout << "H() " << s.transpose() << std::endl;
+//      std::cout << "sonQ " << s_on_Q[0] << " " << s_on_Q[1] << " " << s_on_Q[2] << " " << s_on_Q[3] << " " << s_on_Q[4] << std::endl;
+
+      E_Vector_d ret = E_Vector_d::Zero();
+      E_Vector_d F = E_Vector_d::Zero();
+      E_Vector_d X = E_Vector_d::Zero();
+
+      const WPoint_D& wp0 = cell[0]->m_center_S;
+      for(int i=0; i<d(); ++i)
+      {
+        const WPoint_D& wi = cell[i+1]->m_center_S;
+        for(int j=0; j<D(); ++j)
+        {
+          ret(i) += 2*(wi.point()[j]-wp0.point()[j])*s_on_Q[j];
+          X(i) += 2*(wi.point()[j]-wp0.point()[j])*s_on_Q[j];
+          ret(i) += wp0.point()[j]*wp0.point()[j] - wi.point()[j]*wi.point()[j];
+          F(i) += wp0.point()[j]*wp0.point()[j] - wi.point()[j]*wi.point()[j];
+        }
+        ret(i) += wi.weight() - wp0.weight();
+        F(i) += wi.weight() - wp0.weight();
+      }
+//      std::cout << "X: " << X.transpose() << " F: " << F.transpose() << std::endl;
+      return ret;
+    }
+
+    H(const std::vector<Star_handle>& cell_)
+      : cell(cell_)
+    { }
+  };
+
+  struct J // Jacobian
+  {
+    const std::vector<Star_handle>& cell;
+
+    const int d() const { return dDim::value; }
+    const int D() const { return DDim::value; }
+
+    // the matrix is the jacobian of the system :
+    //  d(p,p0) - w0 = d(p,pi) - wi
+    //  p \in Q
+    // that is : J(i,j) = \frac{\partial \sum_k 2*(p_k-pi_k)x_j}{\partial p_j}
+    // where the x_j are x x² (if d=1), x y x² xy y² (if d=2), etc.
+
+    E_Matrix_d operator()(const E_Vector_d& s) const
+    {
+      E_Matrix_d m;
+      WPoint_D wp0 = cell[0]->m_center_S;
+//      std::cout << "w0: " << cell[0]->index() << " || ";
+//      std::cout << wp0.point()[0] << " " << wp0.point()[1] << " ";
+//      std::cout << wp0.point()[2] << " " << wp0.point()[3] << " ";
+//      std::cout << wp0.point()[4] << std::endl;
+
+      //the easy coefficients : those in the part x y z of \tilde{P}
+      for(int i=0; i<d(); ++i)
+      {
+        WPoint_D wpi = cell[i+1]->m_center_S;
+
+//        std::cout << "wi: " << cell[i+1]->index() << " || ";
+//        std::cout << wpi.point()[0] << " " << wpi.point()[1] << " ";
+//        std::cout << wpi.point()[2] << " " << wpi.point()[3] << " ";
+//        std::cout << wpi.point()[4] << std::endl;
+
+        for(int j=0; j<d(); ++j) // x y z...
+          m(i,j) = 2.*(wpi.point()[j] - wp0.point()[j]);
+      }
+
+      // and now for the hard part: x² xy xz y² yz z² etc...
+      for(int l=0; l<d(); ++l) // lines of m (the function)
+      {
+        const WPoint_D wpl = cell[l+1]->m_center_S;
+        for(int c=0; c<d(); ++c) // columns of m (the derivative)
+        {
+          //ij are the indices of the Q matrix
+          //max is the number of entries at line i (max=d-i)
+          //count is the number of entries visited (lexico order)
+          int i=0, j=0, max=d(), count=0;
+          while(max > 0)
+          {
+            assert(i<d() && j<d());
+            FT coeff = 0.;
+            if(i == c) // the line Q(i,.) : x_i*x_?
+            {
+              if(j == c) // the coeff Q(i,j) is x_i*x_i
+                coeff = 2*s(i);
+              else
+                coeff = s(j); // the coeff Q(i,j) is x_i*x_j
+            }
+            else if(j == c) // the coeff Q(i,j) is x_j*x_? ('?' can't be i)
+              coeff = s(i);
+
+            assert(d()+count < D());
+            // "+d" since we've added the easy part already (the d first)
+            m(l, c) += 2.*(wpl.point()[d()+count]-wp0.point()[d()+count])*coeff;
+
+            j++; count++; // switch from x_i*x_j to x_i*x_{j+1}
+            if(j >= max) // reached the end of a line of Q
+            {
+              max--; i++; j=i; // switch from x_i*x_d to x_{i+1}*x_{i+1}
+            }
+          }
+        }
+      }
+      return m;
+    }
+
+    J(const std::vector<Star_handle>& cell_)
+      : cell(cell_)
+    { }
+  };
+
+  bool is_inside_power_cell(const Point_D& sol,
+                            const std::vector<Star_handle>& cell,
+                            const std::vector<Star_handle>& all_stars) const
+  {
+    std::cout << "Check if the point is in the Pcell of the simplex" << std::endl;
+
+    typename KD::Squared_distance_d csd = KD().squared_distance_d_object();
+
+    const WPoint_D& wp0 = cell[0]->m_center_S;
+    FT sq_d = csd(sol, wp0.point()) - wp0.weight();
+    for(int i=0; i<d(); i++)
+    {
+      const WPoint_D& wpi = cell[i+1]->m_center_S;
+      FT sq_dd = csd(sol, wpi.point()) - wpi.weight();
+//      std::cout << "DIST: " << sq_d << " " << sq_dd << std::endl;
+      if(std::abs(sq_d-sq_dd)/sq_d > 1e-5) // sq_d might be null, I guess... fixme
+      {
+        std::cout.precision(20);
+        std::cout << "WARNING: dds don't agree (sol isn't in the dual) " << sq_d << " " << sq_dd << std::endl;
+      }
+    }
+
+    typename std::vector<Star_handle>::const_iterator sit = all_stars.begin();
+    typename std::vector<Star_handle>::const_iterator send = all_stars.end();
+    for(; sit!=send; ++sit)
+    {
+      Star_handle si = *sit;
+      if(has(cell, si->index())) // vertex belongs to the simplex 'cell'
+        continue;
+
+      WPoint_D wpi = si->m_center_S;
+      FT sq_dv = csd(sol, wpi.point()) - wpi.weight();
+      if(std::abs(sq_dv-sq_d)/sq_d>1e-5 && sq_dv < sq_d) //fixme /sq_d
+      {
+        // not in the power cell of Pi
+        std::cout << "closer : " << si->index() << " " << sq_dv;
+        std::cout << " ref: " << sq_d << std::endl;
+        return false;
+      }
+    }
+    std::cout << "inside the power cells!" << std::endl;
+    return true;
+  }
+
+
+  bool newton(Point_D& sol_on_Q,
+              const std::vector<Star_handle>& cell,
+              const std::vector<Star_handle>& all_stars) const
+  {
+    if(!is_support_intersecting(cell, sol_on_Q)) // get the approximation
+      return false;
+
+    std::cout << "approximation: " << sol_on_Q[0] << " " << sol_on_Q[1] << std::endl;
+    E_Vector_d vsol;
+    for(int i=0; i<d(); ++i)
+      vsol(i) = sol_on_Q[i];  // ugly 'projection' on the paraboloid
+                              // of the intersection with the tangent plane
+
+    FT err = 1e-4;
+    H h(cell); // compute the error
+    J j(cell); // compute the jacobian
+
+    int max = 1e3, count = 0;
+    E_Vector_d h_err = h(vsol);
+    E_Matrix_d jac = j(vsol);
++
+    E_Vector_d previous_vsol = vsol;
+    FT step = 1e30;
+
+    while(h_err.norm() > err && ++count < max)
+    {
+      if(std::abs(jac.determinant())<1e-10)
+      {
+        std::cout << "jacobian det is null" << std::endl;
+        assert(0);
+        return false; // aborting the refinement of this cell...
+      }
+
+      previous_vsol = vsol;
+
+      vsol = vsol - jac.inverse()*h(vsol);
+      jac = j(vsol);
+      h_err = h(vsol);
+//      std::cout << "J det (no tinkerino): " << jac.determinant() << " ";
+
+      step = (vsol-previous_vsol).norm();
+
+//      std::cout << "jac: " << std::endl << jac << std::endl;
+//      std::cout << "J det: " << jac.determinant() << " ";
+//      std::cout << "vsol: " << vsol.transpose() << " ";
+//      std::cout << "h: " << h_err.transpose() << " (" << h_err.norm() << ")" << std::endl;
+
+//      std::cout << std::min(1e3, jac.determinant()) << " ";
+//      std::cout << std::min(1e3, h_err.norm()) << std::endl;
+    }
+
+    if(count==max)
+    {
+      std::cout << "didn't converge in " << max << " iterations.";
+      std::cout << "h: " << h_err.transpose() << std::endl;
+//      assert(0);
+      return false;
+    }
+    else
+      std::cout << "converged to: " << vsol.transpose() << std::endl;
+
+    Point_d vsolp = Kd().construct_point_d_object()(d(), vsol.data(), vsol.data()+d());
+    sol_on_Q  = to_Q(vsolp);
+    std::cout << "final solution : " << vsol.transpose() << " in " << count << " iterations" << std::endl;
+    std::cout << "final jac/h: " << jac.determinant() << " " << h_err.norm() << std::endl;
+
+    return is_inside_power_cell(sol_on_Q, cell, all_stars);
+  }
+
+  bool compute_dual_intersection(Point_D& sol_on_Q,
+                                 const std::vector<Star_handle>& cell,
+                                 const std::vector<Star_handle>& all_stars) const
+  {
+    // we are looking for the intersection of the dual of to_S(fch) with Q
+    // we start with the approximation intersection of dual(to_S(fch)) with T
+    if(newton(sol_on_Q, cell, all_stars))
+    {
+      // Newton's method to (try to) find precisely the root on Q
+//      if(is_intersection_point_in_power_cell(sol_on_Q))
+          return true;
+    }
+    std::cout << "dual does not intersect...? " << std::endl;
+    return false;
+  }
+
+  // Maintenance ---------------------------------------------------------------
   std::size_t clean()
   {
     typedef typename std::pair<Point_d, int> PPoint_d;
@@ -494,7 +847,7 @@ public:
       insert_to_star(backup[i].first, backup[i].second, false);
 
     return (nbv - backup.size());
- }
+  }
 
   void reset()
   {
